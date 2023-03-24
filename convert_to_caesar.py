ed1856b000bd36c0d666f7aa94ee624b90c4d2e1
Ramanavami <48641207+moriishika@users.noreply.github.com>
2023-03-24 04:12:12 +0000

    removed comments

--- convert_to_caesar.py
+++ convert_to_caesar.py
@@ -1,44 +1,3 @@
-# define a function called convert_to_Caesar
-#   The function has one argument called input
-#   input only accept string
-#   iterate each letter from the input
-#   check the ascii value
-#       if the ascii value is bigger or equal to 065 and lesser or equal to 090 
-#     do nothing
-# if the ascii value is bigger or equal to 097 and lesser or equal to 122
-#   convert to uppercase
-# if the letter is dot (.)
-#   convert it to X
-# other than that
-#   dont concate the character
-
-# return the string
-
-
-# TEST
-
-# 1. full number - 876623982382378248 - nothing
-# 2. other data type - 123838138 - prompt the error message
-# 3. Full unique charachter - !@#$%^&*()_?<":}{ - nothing"
-# 4. full small character - "aku suka banget kamu yang cantikkkk"
-# 5. full uppercase - "AKU SUKA BANGET KAMU YANG CANTIKKKKK"
-# 6. mixed of small, upperr and unique - "4kU SuK4 B4nG3T s4M4 K4mU!@#$%^&*()_+{{}|." - kusukbngtsmkmux
-# 7. mixed of number and unique - 1234567890!@#$%^&*()_+{{}|:"M<>?/`"}
-# 8. mixed of small and upper - AkUSuKaKaMU
-
-# def convert_to_Caesar(input):
-#   result = ""
-#   if isinstance(input, str) != True:
-#       return "Bro, string only ;-;"
-#   for letter in input:
-#     if ord(letter) >= 65 and ord(letter) <= 90:
-#       result += letter
-#     elif ord(letter) >= 97 and ord(letter) <= 122:
-#       result += letter.upper()
-#     elif letter == ".":
-#       result+="X"
-#   return result
-
 def convert_to_Caesar(input):
   """
     Convert text that suitable for caesar encryption
@@ -67,16 +26,6 @@ def convert_to_Caesar(input):
     elif ord(letter) >= 97 and ord(letter) <= 122:
       result+=letter.upper()
   return result
-
-  
-# print(convert_to_Caesar("876623982382378248"))
-# print(convert_to_Caesar(87662398238237824))
-# print(convert_to_Caesar("!@#$%^&*()_?<\":}{"))
-# print(convert_to_Caesar("aku suka banget kamu yang cantikkkk"))
-# print(convert_to_Caesar("AKU SUKA BANGET KAMU YANG CANTIKKKKK"))
-# print(convert_to_Caesar("4kU SuK4 B4nG3T s4M4 K4mU!@#$%^&*()_+{{}|."))
-# print(convert_to_Caesar("1234567890!@#$%^&*()_+{{}|:\"M<>?/`\"}"))
-# print(convert_to_Caesar("AkUSuKaKaMU"))
 
 
     
--- encrypt_caesar.py
+++ encrypt_caesar.py
@@ -1,60 +1,6 @@
 from convert_to_caesar import convert_to_Caesar
 import re
 
-# needed values will be the alphabets and the shift value
-# the alphabets has to be converted to uppercase without any unique character and numbers
-# take two inputs: shift number and the text
-# if one of the value is empty then give an error message such as  "Cannot be emptied"
-# if two of the values are available
-# check the value of shift number - only number can be inserted
-# if it is not a number - give an error message such as  "only number can be inserted"
-# if the number is more than 26 or lesser than -26 -give an error
-# message such as "The limit is 26 or -26"
-# if the number is not integer than prompt - only able to receives an integer number
-# or number can be rounded to floor number
-# other unexpected error should be prompt a message "Unexpected input - please input                 integer number"
-# check the value of the text with conver to caesar function
-# if the fucntion gives an error message - display the error message given by the function
-# create a list of uppercased alphabets
-# create a list of uppercased alphabets that shifted based on the shift number
-# duplicate list and remove the elements as many as the shift number from the     very last
-# duplicate another and keep the elements as many as the shift number from        the very last
-# concate both of duplications to be shifted list
-# iterate every letter of the converted string from convert to caesar function
-# while iterating the converted text iterate through shifted list
-# get the position of the letter based on the alphabets list
-# concate the empty string with the letter from the ordered alphabets list        that based on shiftedList letter position
-# display the result
-
-# shift value
-# use alphabets or unique characters
-# input float number
-# input number with double dot
-# input more than the limit
-# input empty value
-# Number has to be converted to characters
-
-# alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-# def encrypt_caesar(shiftNumber, text):
-#   if shiftNumber == 0:
-#     return "Unable to receive 0"
-#   elif isinstance(shiftNumber, int) != True:
-#     return "Only number can be received"
-#   elif shiftNumber < -26 or shiftNumber > 26:
-#     return "The limit shift is -26 or 26"
-
-#   result = ""
-#   convertedText = convert_to_Caesar(text)
-#   shiftedList = alphabets[-shiftNumber:] + alphabets[:-shiftNumber]
-#   for letter in convertedText:
-#     for position, shiftedLetter in enumerate(shiftedList):
-#       if letter == shiftedLetter:
-#         result+= alphabets[position]
-
-#   return result
-
-
 def encrypt_caesar(shiftNumber, text, method):
     """
     Encrypt text based on caesar cypher algorithm
